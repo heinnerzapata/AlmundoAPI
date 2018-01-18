0cd5ee6ced9a463e0a87e6ad4e8d8b18824d5d4b
Heinner Zapata Escarraga <heinner.zapata@globant.com>
2018-01-18 17:46:14 -0500

    fix cors

--- server.js
+++ server.js
@@ -19,13 +19,10 @@ app.get ('/hola:name', (req, res) => {
 })
 */
 
-app.get('/', function(req, res) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,contenttype'); // If needed
-    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
-
-    res.send('cors problem fixed:)');
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 
 app.get('/hotels', (req, res) => {
